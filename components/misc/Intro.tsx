b6198529e5f8562edf1b6ce0ca7622e82c66dc21
Arpan Bhandari <arpanbhandari@Arpans-MacBook-Air.local>
2023-11-11 18:13:21 +0545

    Made image undraggable

--- components/misc/Intro.tsx
+++ components/misc/Intro.tsx
@@ -17,14 +17,14 @@ export default function Intro() {
           </span>
           <div className="flex gap-4 mt-6">
             <button className="group hover:bg-sky-600 relative bg-sky-700 rounded-lg text-neutral-50 duration-500 font-bold flex justify-start gap-2 items-center p-2 pr-6">
-              <AiFillLinkedin className="text-3xl"/>
+              <AiFillLinkedin className="text-3xl" />
               <span className="border-l-2 px-1">arpan404</span>
               <div className="group-hover:opacity-100 opacity-0 top-16 absolute z-10 inline-block px-3 py-2 text-xs font-medium text-white transition-opacity duration-500 bg-sky-600 rounded-lg shadow-sm before:w-3 before:h-3 before:rotate-45 before:-top-1 before:left-20 before:bg-sky-600 before:absolute">
                 See my profile!
               </div>
             </button>
             <button className="group hover:bg-gray-900 relative bg-gray-800 rounded-lg text-neutral-50 duration-500 font-bold flex justify-start gap-2 items-center p-2 pr-6">
-              <AiFillGithub className="text-3xl"/>
+              <AiFillGithub className="text-3xl" />
               <span className="border-l-2 px-1">arpan404</span>
               <div className="group-hover:opacity-100 opacity-0 top-16 absolute z-10 inline-block px-3 py-2 text-xs font-medium text-white transition-opacity duration-500 bg-gray-900 rounded-lg shadow-sm before:w-3 before:h-3 before:rotate-45 before:-top-1 before:left-20 before:bg-gray-900 before:absolute">
                 Visit my treasury!
@@ -40,6 +40,7 @@ export default function Intro() {
           width={500}
           alt=""
           className="lg:w-[500px]  w-[250px] sm:w-[200px] md:w-[250px]"
+          draggable={false}
         />
       </div>
     </main>
