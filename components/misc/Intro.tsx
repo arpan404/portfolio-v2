603029b13030905ef46c8e9f5f90b08025397ce3
Arpan Bhandari <arpanbhandari@Arpans-MacBook-Air.local>
2023-11-13 17:58:36 +0545

    fixed build failed

--- components/misc/Intro.tsx
+++ components/misc/Intro.tsx
@@ -9,7 +9,7 @@ export default function Intro() {
         <div>
           <h2 className="sm:text-6xl text-gray-50 font-bold text-5xl">
             Hey there!
-            <br /> I'm <span className="font-black text-[#ff8e00]">Arpan</span>.
+            <br /> I&apos;m <span className="font-black text-[#ff8e00]">Arpan</span>.
           </h2>
           <span className="mt-2">
             <h2 className="text-gray-300 lg:text-xl md:text-lg text-[1.01rem] ">
